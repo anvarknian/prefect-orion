c1f24e2f64712cb9024a79af048224f6914d73f5
anvarknian <anvarknian@yandex.ru>
2022-10-13 16:00:02 +0300

    fix

--- flows/test_flow/deployment.py
+++ flows/test_flow/deployment.py
@@ -1,35 +1,49 @@
-from test_flow import my_docker_flow
+import os
+import uuid
+from datetime import datetime
 
 from prefect.deployments import Deployment
 from prefect.filesystems import RemoteFileSystem
 from prefect.infrastructure import DockerContainer
 
-minio_block = RemoteFileSystem(
-    basepath="s3://prefect-flows/test_flow",
+from test_flow import my_docker_flow
+
+flow_identifier = datetime.today().strftime("%Y%m%d%H%M%S-") + str(uuid.uuid4())
+
+bucket_name = os.environ.get("MINIO_PREFECT_FLOWS_BUCKET_NAME")
+artifacts_bucket_name = os.environ.get("MINIO_PREFECT_ARTIFACTS_BUCKET_NAME")
+minio_endpoint = os.environ.get("MINIO_ENDPOINT")
+minio_use_ssl = os.environ.get("MINIO_USE_SSL") == "true"
+minio_scheme = "https" if minio_use_ssl else "http"
+minio_access_key = os.environ.get("MINIO_ACCESS_KEY")
+minio_secret_key = os.environ.get("MINIO_SECRET_KEY")
+endpoint_url=f"{minio_scheme}://{minio_endpoint}"
+
+block_storage = RemoteFileSystem(
+    basepath=f"s3://{bucket_name}/{flow_identifier}",
     key_type="hash",
     settings=dict(
-        use_ssl = False,
-        key = "blablabla",
-        secret = "blablabla",
-        client_kwargs = dict(endpoint_url = "http://minio:9000")
+        use_ssl=minio_use_ssl,
+        key=minio_access_key,
+        secret=minio_secret_key,
+        client_kwargs=dict(endpoint_url=endpoint_url),
     ),
 )
-minio_block.save("minio", overwrite=True)
-
+block_storage.save("s3-storage", overwrite=True)
 
 deployment = Deployment.build_from_flow(
     name="docker-example",
     flow=my_docker_flow,
     storage=RemoteFileSystem.load('minio'),
     infrastructure=DockerContainer(
-        image = 'prefect-orion:2.4.5',
-        image_pull_policy = 'IF_NOT_PRESENT',
-        networks = ['prefect'],
-        env = {
-            "USE_SSL": False,
-            "AWS_ACCESS_KEY_ID": "blablabla",
-            "AWS_SECRET_ACCESS_KEY": "blablabla",
-            "ENDPOINT_URL": 'http://minio:9000',
+        image='prefect-orion:2.4.5',
+        image_pull_policy='IF_NOT_PRESENT',
+        networks=['prefect'],
+        env={
+            "USE_SSL": minio_use_ssl,
+            "AWS_ACCESS_KEY_ID": minio_access_key,
+            "AWS_SECRET_ACCESS_KEY": minio_secret_key,
+            "ENDPOINT_URL": endpoint_url,
         }
     ),
 )
